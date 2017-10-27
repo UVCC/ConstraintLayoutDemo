fe5486c5f5331fe3e0689cd64dafe885093847f3
UVCC <china.uv.cc@gmail.com>
2017-10-27 08:50:20 +0800

    新建

---
+++ app/src/main/java/com/uv/constraintlayoutdemo/CLDMainActivity.kt
@@ -0,0 +1,12 @@
+package com.uv.constraintlayoutdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CLDMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cld_main)
+    }
+}
